d52289f3ff4f937ef3390d6ac7d06a8bbf440246
sikmar97 <sikmar35@gmail.com>
2024-01-20 22:09:42 +0100

    Feat: Added Hero with Three.js and Objloader implemented TODO: CHANGE MODEL

---
+++ src/app/Components/Pages/About.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+    return (
+      <>
+     <div>
+      <h1 className="text-white">About</h1>
+
+      <figure className="four">
+		<img src="https://i.ibb.co/Tw6pNWn/292791834-455641426567696-2643468163026532432-n-removebg-preview.png" alt="" />
+	</figure>
+    </div>
+      </>
+    );
+  }
+  
---
+++ src/app/Components/Pages/Hero.jsx
@@ -0,0 +1,22 @@
+
+export default function Hero() {
+  return (
+    <>
+   
+<div className="headline-container">
+  <div id="text-behind">Apaztron</div>
+  <div id="text-behind-blur">Apaztron</div>
+  <div id="text-front">Apaztron</div>
+</div>
+
+<div className="canvas-container">
+    <canvas id="canvas"></canvas>
+</div>
+
+<div className="more-content">
+</div>
+
+
+    </>
+  );
+}
